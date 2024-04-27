8145b5219eecdebf12ee33410b4549046bf4cea8
rajeshs-raju <rajesh54raju@gmail.com>
2024-04-28 00:25:33 +0530

    Added src and test folders

---
+++ src/test.py
@@ -0,0 +1,22 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(0, 0) == 0
+    assert add(-1, 1) == 0
+    assert add(10,15) == 20
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(0, 0) == 0
+    assert subtract(-1, -1) == 0
+
+if __name__ == "__main__":
+    test_add()
+    test_subtract()
+    print("All tests passed!")
+
