3dd4a695765150f8a94b06d7674626b7143b1ed1
rajeshs-raju <rajesh54raju@gmail.com>
2024-04-28 12:49:28 +0530

    Updated changes

--- src/test.py
+++ src/test.py
@@ -8,13 +8,13 @@ def test_add():
     assert add(2, 3) == 5
     assert add(0, 0) == 0
     assert add(-1, 1) == 0
-    assert add(10,15) == 20
+    assert add(10,15) == 25
 
 def test_subtract():
     assert subtract(5, 3) == 2
     assert subtract(0, 0) == 0
     assert subtract(-1, -1) == 0
-    assert subtract(25,0)  == 15
+    assert subtract(25,0)  == 25
 
 if __name__ == "__main__":
     test_add()
